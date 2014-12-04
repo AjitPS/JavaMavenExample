581cf2fefef7e39ad97ca079882fbce3c0aa0a3b
Ajit Singh <ajitprataps@yahoo.com>
2014-12-04 15:59:44 +0000

    cmt1
    code added

---
+++ src/main/java/com/mypc/javamavenproject1/App.java
@@ -0,0 +1,9 @@
+package com.mypc.javamavenproject1;
+
+import javax.swing.JOptionPane;
+
+public class App {
+ public static void main( String[] args ) {
+  JOptionPane.showMessageDialog(null, "JavaMavenProject1 demo");
+ }
+}
