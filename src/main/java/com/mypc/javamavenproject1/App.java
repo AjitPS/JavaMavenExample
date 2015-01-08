4cea1f1b97584a0c32f047fb915ce4931adf5550
Ajit Singh <ajitprataps@yahoo.com>
2015-01-08 15:37:59 +0000

    cmtNew

--- src/main/java/com/mypc/javamavenproject1/App.java
+++ src/main/java/com/mypc/javamavenproject1/App.java
@@ -1,9 +1,13 @@
 package com.mypc.javamavenproject1;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class App {
  public static void main( String[] args ) {
-  JOptionPane.showMessageDialog(null, "JavaMavenProject1 demo");
+  String fileName= new SimpleDateFormat("yyyyMMddhhmmss'.txt'").format(new Date());
+     
+  JOptionPane.showMessageDialog(null, "JavaMavenProject1 demo, filename with timestamp: "+ fileName);
  }
 }
--- src/main/java/com/mypc/javamavenproject1/Info.java
+++
@@ -1,5 +0,0 @@
-package com.mypc.javamavenproject1;
-
-public class Info {
-    
-}
